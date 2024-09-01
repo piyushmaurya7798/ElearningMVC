737008c23f258fb46e607e76c2431b37bd19e810
piyushmaurya7798 <piyushmaurya7798@gmail.com>
2024-09-02 00:24:40 +0530

    Credentials

--- Program.cs
+++ Program.cs
@@ -29,12 +29,12 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         option.ExpireTimeSpan = TimeSpan.FromMinutes(60);
         option.LoginPath = "/Auth/SignIn";
         option.AccessDeniedPath = "/Auth/SignIn";
-    });
-  //.AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
-  // {
-	 //  options.ClientId = "806000584966-6tou4mcd0qh0r10ugkh8b3r7c8kmv0k8.apps.googleusercontent.com";
-	 //  options.ClientSecret = "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";
-  // });
+    })
+  .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+   {
+       options.ClientId = "806000584966-6tou4mcd0qh0r10ugkh8b3r7c8kmv0k8.apps.googleusercontent.com";
+       options.ClientSecret = "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";
+   });
 
 builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
